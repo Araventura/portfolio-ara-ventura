b7c95f7687644d85878838160666ad63bc9b8366
Ara Ventura <ara.venturafitch@gmail.com>
2024-10-07 14:56:51 -0700

    add home page, and variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import ".App.scss";
+import "./App.scss";
 import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import { BrowserRouter } from "react-router-dom";
 
 import React from "react";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Home />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./Header.scss";
+import { Link } from "react-router-dom";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="header__left">
+        <div>
+          <Link>LI</Link>
+          <Link>GIT</Link>
+          <Link>EM</Link>
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
+      </div>
+      <div className="header__right">
+        <p>©/2024</p>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
