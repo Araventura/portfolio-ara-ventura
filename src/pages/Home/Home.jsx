5626156d118b2586f42dd41acc3c931b52a54a76
Ara Ventura <ara.venturafitch@gmail.com>
2024-10-08 13:44:01 -0700

    add skeleton to home page, add mixins for  screen sizing

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "./Home.scss";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="home__top">
+        <h2 className="home__title">Ara Ventura</h2>
+        <h4 className="home__description">
+          Software Engineer / WordPress Developer
+        </h4>
+        <h4 className="home__open">
+          Currently open for collaborations and freelancing
+        </h4>
+      </div>
+      <div className="home__bottom">
+        <h3 className="home__bottom-text">WORK</h3>
+        <h3 className="home__bottom-text">ABOUT</h3>
+        <h3 className="home__bottom-text">CONTACT</h3>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
