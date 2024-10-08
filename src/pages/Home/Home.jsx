26af9e4f8a519af276422b423f690fcb4320273f
Ara Ventura <ara.venturafitch@gmail.com>
2024-10-08 14:57:30 -0700

    add navbar on the left side

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import "./Home.scss";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div className="home">
       <div className="home__top">
-        <h2 className="home__title">Ara Ventura</h2>
+        <h1 className="home__title">ARA VENTURA</h1>
         <h4 className="home__description">
-          Software Engineer / WordPress Developer
+          Software Engineer / Full Stack Web Developer / WordPress Developer
         </h4>
         <h4 className="home__open">
           Currently open for collaborations and freelancing
         </h4>
       </div>
       <div className="home__bottom">
-        <h3 className="home__bottom-text">WORK</h3>
-        <h3 className="home__bottom-text">ABOUT</h3>
-        <h3 className="home__bottom-text">CONTACT</h3>
+        <Link className="home__bottom-text">WORK</Link>
+        <Link className="home__bottom-text">ABOUT</Link>
+        <Link className="home__bottom-text">CONTACT</Link>
       </div>
     </div>
   );
