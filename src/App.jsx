6934343de5dbbf1a3206ccbda98c961526f8d926
Ara Ventura <ara.venturafitch@gmail.com>
2024-10-08 17:05:09 -0700

    add routes and route for about, contact and work

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
-import { BrowserRouter } from "react-router-dom";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Work from "./components/Work/Work";
 
 import React from "react";
 
@@ -9,7 +13,12 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/work" element={<Work />} />
+      </Routes>
     </BrowserRouter>
   );
 }
