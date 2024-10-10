678a62e5e5282efde6f788d72643fa0eaa305a2e
Ara Ventura <ara.venturafitch@gmail.com>
2024-10-09 19:45:40 -0700

    animation on about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,55 +6,63 @@ function About() {
   return (
     <div className="about">
       <div className="about__text-top">
-        <h1 className="about__title">About</h1>
-        <h2 className="about__subtitle">
-          I'm Ara. A software engineer, maker and problem solver.
-        </h2>
+        <div>
+          <h1 className="about__title">About</h1>
+          <h2 className="about__subtitle">
+            I'm Ara. A software engineer, maker and problem solver.
+          </h2>
+        </div>
       </div>
-      <h4 className="about__tech">
-        HTML / CSS / SCSS / Sass / JavaScript / Node / React / Express / MySQL /
-        Agile Development / DOM / API / Web APIs / User Authentication / OAuth /
-        Heroku / GitHub / Jest / REST APIs / Node.js / Figma / Git / GitHub /
-        WordPress
-      </h4>
+      <div className="about__text-bottom">
+        <div>
+          <h4 className="about__tech">
+            HTML / CSS / SCSS / Sass / JavaScript / Node / React / Express /
+            MySQL / Agile Development / DOM / API / Web APIs / User
+            Authentication / OAuth / Heroku / GitHub / Jest / REST APIs /
+            Node.js / Figma / Git / GitHub / WordPress
+          </h4>
 
-      <p className="about__text">
-        After dedicating over six years to the dental field, I realized that
-        while I enjoyed the precision and problem-solving in dentistry, my true
-        curiosity had always been rooted in technology. I’ve always been
-        fascinated by logic, critical thinking, and the art of finding
-        solutions—elements that naturally led me to explore the world of
-        software engineering.
-      </p>
+          <p className="about__text">
+            After dedicating over six years to the dental field, I realized that
+            while I enjoyed the precision and problem-solving in dentistry, my
+            true curiosity had always been rooted in technology. I’ve always
+            been fascinated by logic, critical thinking, and the art of finding
+            solutions—elements that naturally led me to explore the world of
+            software engineering.
+          </p>
 
-      <p className="about__text">
-        What started as a curiosity quickly became a passion, especially when I
-        discovered the thrill of debugging. There’s something deeply satisfying
-        about tracing the problem, untangling the code, and watching things come
-        to life. I also found myself drawn to the aesthetics of front-end
-        development, where styling and visual design meet functional code—giving
-        me a chance to blend creativity with logic.
-      </p>
+          <p className="about__text">
+            What started as a curiosity quickly became a passion, especially
+            when I discovered the thrill of debugging. There’s something deeply
+            satisfying about tracing the problem, untangling the code, and
+            watching things come to life. I also found myself drawn to the
+            aesthetics of front-end development, where styling and visual design
+            meet functional code—giving me a chance to blend creativity with
+            logic.
+          </p>
 
-      <p className="about__text">
-        But my love for coding didn’t stop at the front-end. Once I delved into
-        the backend, I realized how much I enjoyed the intricacies of building
-        the structure that powers applications. Now, I’m equally passionate
-        about both, constantly learning and expanding my skill set. Whether it’s
-        refining a user interface or solving complex backend challenges, I’m
-        driven by the opportunity to learn something new every day and
-        contribute to innovative solutions.
-      </p>
+          <p className="about__text">
+            But my love for coding didn’t stop at the front-end. Once I delved
+            into the backend, I realized how much I enjoyed the intricacies of
+            building the structure that powers applications. Now, I’m equally
+            passionate about both, constantly learning and expanding my skill
+            set. Whether it’s refining a user interface or solving complex
+            backend challenges, I’m driven by the opportunity to learn something
+            new every day and contribute to innovative solutions.
+          </p>
 
-      <p className="about__text">
-        As I transition from dentistry to tech, I bring a meticulous eye for
-        detail and a passion for crafting beautiful and efficient solutions. My
-        journey has just begun, and I’m excited to see where this path takes me.
-      </p>
+          <p className="about__text">
+            As I transition from dentistry to tech, I bring a meticulous eye for
+            detail and a passion for crafting beautiful and efficient solutions.
+            My journey has just begun, and I’m excited to see where this path
+            takes me.
+          </p>
 
-      <Link to={"/contact"}>
-        <button>Contact Me</button>
-      </Link>
+          <Link to={"/contact"}>
+            <button>Contact Me</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
