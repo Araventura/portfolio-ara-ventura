22df0c0bd80adf81fe2a2f5d126ce18a2b4eb1f3
Ara Ventura <ara.venturafitch@gmail.com>
2024-10-15 16:34:00 -0700

    map through each video, change name of each object

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -14,18 +14,22 @@ function Work() {
       <h2 className="work__title">WORK</h2>
       <p className="work__description">
         This is a showcase of my work. The world is constantly evolving and so
-        am I. I'm still learning and gaining new skills everyday.
+        am I. I'm learning and gaining new skills everyday.
       </p>
       <div className="cards">
-        <div className="card">
-          <img src="" alt="" className="thumbnail" />
-          <video src=""></video>
-          <div className="card-info">
-            <h4 className="card__title">Card Title</h4>
-            <p className="card__year">Card Month/Year</p>
-            <p className="card__description">Card Description</p>
+        {videos.map(() => (
+          <div className="card">
+            <div className="card__video">
+              <img src="" alt="" className="thumbnail" />
+              <video src=""></video>
+            </div>
+            <div className="card-info">
+              <h4 className="card__title">Card Title</h4>
+              <p className="card__year">Card Month/Year</p>
+              <p className="card__description">Card Description</p>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
       <Link to={"/contact"}>
         <button>Contact Me</button>
