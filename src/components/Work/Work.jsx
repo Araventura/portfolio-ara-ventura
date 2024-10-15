f54e7768ffad839068ddb22a2507936c1f490461
Ara Ventura <ara.venturafitch@gmail.com>
2024-10-15 13:59:24 -0700

    add json file

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import "./Work.scss";
 
 function Work() {
-  return <div>Work</div>;
+  return (
+    <div className="work">
+      <h4>WORK</h4>
+      <p>
+        This is a showcase of my work. The world is constantly evolving and so
+        am I. I'm still learning and gaining new skills everyday
+      </p>
+      <div className="cards">
+        <div className="card">
+          <img src="" alt="" className="thumbnail" />
+          <video src=""></video>
+          <div className="card-info">
+            <p>Card Title</p>
+            <p>Card Month/Year</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Work;
