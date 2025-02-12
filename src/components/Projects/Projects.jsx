86d0926e697aa8ce7fd08acaba54b53c29bdeb27
Ara Ventura <ara.venturafitch@gmail.com>
2025-02-11 16:23:37 -0800

    add import files to project component

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Projects.scss";
+import { Link } from "react-router-dom";
+import workImage from "../../assets/images/mobile-tablet.png";
+import dentaShiftImage from "../../assets/images/dentashift.png";
+import purelyChic from "../../assets/images/purelyChic.png";
+
+function Projects() {
+  return <div>Projects</div>;
+}
+
+export default Projects;
