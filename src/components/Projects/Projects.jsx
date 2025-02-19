e42cf7a622549d3a98c6d881613eca457fa74544
Ara Ventura <ara.venturafitch@gmail.com>
2025-02-19 12:51:19 -0800

    add swiper to projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,59 +8,74 @@ import "swiper/css";
 
 function Projects() {
   return (
-    <ul className="home__list">
-      <li className="home__list-item">
-        <img
-          className="home__bottom-wrapper__img"
-          src={workImage}
-          alt="A mobile and tablet simulation"
-        />
-        <div className="home__list-wrapper">
-          <h3 className="home__list-title">InStock - Practice </h3>
-          <h4 className="home__list-description">
-            JavaScript, React, MySQL, Express, Rest APIs
-          </h4>
-          <h4 className="home__list-description">
-            A web application where the customer can manage Warehouses and
-            Inventory.
-          </h4>
-        </div>
-      </li>
-      <li className="home__list-item">
-        <img
-          className="home__bottom-wrapper__img"
-          src={dentaShiftImage}
-          alt="A mobile and tablet simulation"
-        />
-        <div className="home__list-wrapper">
-          <h3 className="home__list-title">Denta Shift - Practice</h3>
-          <h4 className="home__list-description">
-            JavaScript, React, MySQL, Express, Rest APIs
-          </h4>
-          <h4 className="home__list-description">
-            A web application where dentists can find a dental professional
-            based on their offices needs.
-          </h4>
-        </div>
-      </li>
-      <li className="home__list-item">
-        <img
-          className="home__bottom-wrapper__img"
-          src={purelyChic}
-          alt="A mobile and tablet simulation"
-        />
-        <div className="home__list-wrapper">
-          <h3 className="home__list-title">Purely Chic - Practice</h3>
-          <h4 className="home__list-description">
-            Vite, Sass, React.js, Axios, Node.js and CORS.
-          </h4>
-          <h4 className="home__list-description">
-            A web application where users can find beauty products that are
-            eco-friendly.
-          </h4>
-        </div>
-      </li>
-    </ul>
+    <div>
+      <ul className="home__list">
+        <Swiper
+          spaceBetween={2}
+          slidesPerView={1}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide>
+            <li className="home__list-item">
+              <img
+                className="home__bottom-wrapper__img"
+                src={workImage}
+                alt="A mobile and tablet simulation"
+              />
+              <div className="home__list-wrapper">
+                <h3 className="home__list-title">InStock - Practice </h3>
+                <h4 className="home__list-description">
+                  JavaScript, React, MySQL, Express, Rest APIs
+                </h4>
+                <h4 className="home__list-description">
+                  A web application where the customer can manage Warehouses and
+                  Inventory.
+                </h4>
+              </div>
+            </li>
+          </SwiperSlide>
+          <SwiperSlide>
+            <li className="home__list-item">
+              <img
+                className="home__bottom-wrapper__img"
+                src={dentaShiftImage}
+                alt="A mobile and tablet simulation"
+              />
+              <div className="home__list-wrapper">
+                <h3 className="home__list-title">Denta Shift - Practice</h3>
+                <h4 className="home__list-description">
+                  JavaScript, React, MySQL, Express, Rest APIs
+                </h4>
+                <h4 className="home__list-description">
+                  A web application where dentists can find a dental
+                  professional based on their offices needs.
+                </h4>
+              </div>
+            </li>
+          </SwiperSlide>
+          <SwiperSlide>
+            <li className="home__list-item">
+              <img
+                className="home__bottom-wrapper__img"
+                src={purelyChic}
+                alt="A mobile and tablet simulation"
+              />
+              <div className="home__list-wrapper">
+                <h3 className="home__list-title">Purely Chic - Practice</h3>
+                <h4 className="home__list-description">
+                  Vite, Sass, React.js, Axios, Node.js and CORS.
+                </h4>
+                <h4 className="home__list-description">
+                  A web application where users can find beauty products that
+                  are eco-friendly.
+                </h4>
+              </div>
+            </li>
+          </SwiperSlide>
+        </Swiper>
+      </ul>
+    </div>
   );
 }
 
