84018a525522bbc7e88ad2b4a79338c92a302568
Ara Ventura <ara.venturafitch@gmail.com>
2025-03-03 21:06:47 -0800

    removed text from about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -29,50 +29,16 @@ function About() {
           </div>
           <div className="about__skills">
             <h3 className="about__skills-title">Skills & Technologies</h3>
-            {skillsData.map((i) => {
-              return (
-                <h4 key={i.key} className="about__skills-tech">
-                  ✓ {i.name}
-                </h4>
-              );
-            })}
+            <div className="about__skills-grid">
+              {skillsData.map((i) => {
+                return (
+                  <h4 key={i.key} className="about__skills-tech">
+                    ✓ {i.name}
+                  </h4>
+                );
+              })}
+            </div>
           </div>
-
-          <p className="about__text">
-            After dedicating over six years to the dental field, I realized that
-            while I enjoyed the precision and problem-solving in dentistry, my
-            true curiosity had always been rooted in technology. I’ve always
-            been fascinated by logic, critical thinking, and the art of finding
-            solutions—elements that naturally led me to explore the world of
-            software engineering.
-          </p>
-
-          <p className="about__text">
-            What started as a curiosity quickly became a passion, especially
-            when I discovered the thrill of debugging. There’s something deeply
-            satisfying about tracing the problem, untangling the code, and
-            watching things come to life. I also found myself drawn to the
-            aesthetics of front-end development, where styling and visual design
-            meet functional code—giving me a chance to blend creativity with
-            logic.
-          </p>
-
-          <p className="about__text">
-            But my love for coding didn’t stop at the front-end. Once I delved
-            into the backend, I realized how much I enjoyed the intricacies of
-            building the structure that powers applications. Now, I’m equally
-            passionate about both, constantly learning and expanding my skill
-            set. Whether it’s refining a user interface or solving complex
-            backend challenges, I’m driven by the opportunity to learn something
-            new every day and contribute to innovative solutions.
-          </p>
-
-          <p className="about__text">
-            I bring a meticulous eye for detail and a passion for crafting
-            beautiful and efficient solutions. My journey has just begun, and
-            I’m excited to see where this path takes me.
-          </p>
-
           <Link to={"/contact"}>
             <button>Contact Me</button>
           </Link>
