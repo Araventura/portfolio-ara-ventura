c2825b60a8b4a2e960d3810cd082765ca3be69bf
Ara Ventura <ara.venturafitch@gmail.com>
2025-03-06 16:07:39 -0800

    add divs to improve layout on about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,24 +9,28 @@ function About() {
       <div className="about__text-top">
         <div>
           <h1 className="about__title">About</h1>
-          <div className="about__wrapper">
-            <img className="about__image" src={profileImg} alt="" />
+          <div className="about__top-wrapper">
+            <div className="about__wrapper">
+              <img className="about__image" src={profileImg} alt="" />
+            </div>
+            <div className="about__bottom-wrapper">
+              <h2 className="about__subtitle">
+                Nice to see you, I'm Ara. A Junior Software Engineer who
+                specializes in React, Express, MySQL and JavaScript.
+              </h2>
+              <div className="about__tech-backdrop">
+                <h4 className="about__tech-emoji">🏆</h4>
+                <h3 className="about__tech">
+                  Most recent achievement: Certification in Artificial
+                  Intelligence (AIC™) Nov 2024
+                </h3>
+              </div>
+            </div>
           </div>
-          <h2 className="about__subtitle">
-            Nice to see you, I'm Ara. A Junior Software Engineer who specializes
-            in React, Express, MySQL and JavaScript.
-          </h2>
         </div>
       </div>
       <div className="about__text-bottom">
         <div>
-          <div className="about__tech-backdrop">
-            <h4 className="about__tech-emoji">🏆</h4>
-            <h3 className="about__tech">
-              Most recent achievement: Certification in Artificial Intelligence
-              (AIC™) Nov 2024
-            </h3>
-          </div>
           <div className="about__skills">
             <h3 className="about__skills-title">Skills & Technologies</h3>
             <div className="about__skills-grid">
