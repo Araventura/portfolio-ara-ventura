856b923a9a27fdd33f444ab08ce5abf2d50832ae
Ara Ventura <ara.venturafitch@gmail.com>
2025-03-07 18:58:31 -0800

    improved resposiveness on resolutions larger than 1200px

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,30 +8,33 @@ import emailImg from "../../assets/icons/mail.svg";
 function Header() {
   return (
     <div className="header">
-      <div className="header__left">
-        <div className="header__socials">
-          <Link
-            to="https://www.linkedin.com/in/ara-ventura/"
-            className="header__link"
-          >
-            <img
-              className="header__logo"
-              src={linkedinImg}
-              alt="LinkedIn logo"
-            />
-          </Link>
-          <Link to="https://github.com/Araventura" className="header__link">
-            <img className="header__logo" src={githubImg} alt="GitHub logo" />
-          </Link>
-          <Link to={"/contact"} className="header__link">
-            <img className="header__logo" src={emailImg} alt="" />
+      <div className="header__wrapper">
+        <div className="header__left">
+          <div className="header__socials">
+            <Link
+              to="https://www.linkedin.com/in/ara-ventura/"
+              className="header__link"
+            >
+              <img
+                className="header__logo"
+                src={linkedinImg}
+                alt="LinkedIn logo"
+              />
+            </Link>
+            <Link to="https://github.com/Araventura" className="header__link">
+              <img className="header__logo" src={githubImg} alt="GitHub logo" />
+            </Link>
+            <Link to={"/contact"} className="header__link">
+              <img className="header__logo" src={emailImg} alt="" />
+            </Link>
+          </div>
+        </div>
+
+        <div className="header__right">
+          <Link to={"/"} className="header__home">
+            HOME
           </Link>
         </div>
-      </div>
-      <div className="header__right">
-        <Link to={"/"} className="header__home">
-          HOME
-        </Link>
       </div>
     </div>
   );
--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -14,49 +14,53 @@ import { Navigation, Mousewheel, Keyboard } from "swiper/modules";
 function Work() {
   return (
     <div className="work">
-      <h2 className="work__title">WORK</h2>
-      <p className="work__description">
-        This is a showcase of my latest projects. Swipe to see them. Enjoy!
-      </p>
+      <div className="work__wrapper-top">
+        <h2 className="work__title">WORK</h2>
+        <p className="work__description">
+          This is a showcase of my latest projects. Swipe to see them. Enjoy!
+        </p>
+      </div>
 
       {/* -Swiper- */}
-      <ul className="work__list">
-        <Swiper
-          spaceBetween={2}
-          slidesPerView={1}
-          cssMode={true}
-          navigation={true}
-          mousewheel={true}
-          keyboard={true}
-          modules={[Navigation, Mousewheel, Keyboard]}
-          className="swiper"
-        >
-          {projects.map((project) => (
-            <SwiperSlide>
-              <li key={project.id} className="work__list-item">
-                <img
-                  className="work__bottom-wrapper__img"
-                  src={project.image}
-                  alt="A mobile and tablet simulation"
-                />
-                <div className="work__list-wrapper">
-                  <h3 className="work__list-title">{project.title}</h3>
-                  <h4 className="work__list-description">
-                    {project.technologies}
-                  </h4>
-                  <h4 className="work__list-description">
-                    {project.description}
-                  </h4>
-                </div>
-              </li>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </ul>
-      <div className="work__wrapper">
-        <Link to={"/contact"}>
-          <button className="work__button">Contact Me</button>
-        </Link>
+      <div className="work__wrapper-top">
+        <ul className="work__list">
+          <Swiper
+            spaceBetween={2}
+            slidesPerView={1}
+            cssMode={true}
+            navigation={true}
+            mousewheel={true}
+            keyboard={true}
+            modules={[Navigation, Mousewheel, Keyboard]}
+            className="swiper"
+          >
+            {projects.map((project) => (
+              <SwiperSlide>
+                <li key={project.id} className="work__list-item">
+                  <img
+                    className="work__bottom-wrapper__img"
+                    src={project.image}
+                    alt="A mobile and tablet simulation"
+                  />
+                  <div className="work__list-wrapper">
+                    <h3 className="work__list-title">{project.title}</h3>
+                    <h4 className="work__list-description">
+                      {project.technologies}
+                    </h4>
+                    <h4 className="work__list-description">
+                      {project.description}
+                    </h4>
+                  </div>
+                </li>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </ul>
+        <div className="work__wrapper">
+          <Link to={"/contact"}>
+            <button className="work__button">Contact Me</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
